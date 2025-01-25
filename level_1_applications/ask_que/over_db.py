b4d40d07b2fa4632359c61bd021dc0d89369de21
tiilon <tiilon42@gmailcom>
2025-01-25 16:44:07 +0000

    finished ask_que

--- level_1_applications/ask_que/over_db.py
+++ level_1_applications/ask_que/over_db.py
@@ -1,2 +1,47 @@
 from langchain_ollama import ChatOllama
+from langchain_community.utilities import SQLDatabase
+from langchain.chains import create_sql_query_chain
+from langchain_community.tools.sql_database.tool import QuerySQLDataBaseTool
+from operator import itemgetter
+
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnablePassthrough
+
 llm = ChatOllama(model="llama3.2", temperature=0.9)
+sqlite_db_path = "./data/street_tree_db.sqlite"
+
+db = SQLDatabase.from_uri(f"sqlite:///{sqlite_db_path}")
+
+answer_prompt = PromptTemplate.from_template(
+    """Given the following user question, 
+    corresponding SQL query, and SQL result, 
+    answer the user question.
+
+Question: {question}
+SQL Query: {query}
+SQL Result: {result}
+Answer: """
+)
+
+write_query = create_sql_query_chain(llm, db)
+
+execute_query = QuerySQLDataBaseTool(db=db)
+
+#chain = write_query | execute_query
+
+#chain.invoke({"question": "List the species of trees that are present in San Francisco"})
+
+# chain = (
+#     RunnablePassthrough.assign(query=write_query).assign(
+#         result=itemgetter("query") | execute_query
+#     )
+#     | answer_prompt
+#     | llm
+#     | StrOutputParser()
+# )
+
+chain = create_sql_query_chain(llm, db)
+response = chain.invoke({"question": "List the species of trees that are present in San Francisco"})
+
+print(response)
--- level_1_applications/ask_que/over_pdf.py
+++ level_1_applications/ask_que/over_pdf.py
@@ -1,2 +1,52 @@
 from langchain_ollama import ChatOllama
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_chroma import Chroma
+from langchain_ollama import OllamaEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain.chains import create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.prompts import ChatPromptTemplate
+
 llm = ChatOllama(model="llama3.2", temperature=0.9)
+
+file_path = "./data/Be_Good.pdf"
+
+loader = PyPDFLoader(file_path)
+
+docs = loader.load()
+
+embeddings = OllamaEmbeddings(model="llama3.2")
+
+text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+
+splits = text_splitter.split_documents(docs)
+
+vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings)
+
+retriever = vectorstore.as_retriever()
+
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        ("human", "{input}"),
+    ]
+)
+# create_stuff_documents_chain - (Takes a List of Documents:,Formats into a Prompt:,Passing to an LLM:,Fit within Context Window:)
+question_answer_chain = create_stuff_documents_chain(llm, prompt)
+
+# create_retrieval_chain - (Receiving a User Inquiry:,Use a Retriever to Fetch Documents:,Pass Information to an LLM:,Generating a Response:)
+rag_chain = create_retrieval_chain(retriever, question_answer_chain)
+
+results = rag_chain.invoke({"input": "What is this article about?"})
+
+print(results["answer"])
